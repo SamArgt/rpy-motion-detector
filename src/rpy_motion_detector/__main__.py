9589d437fa0a77f92265b678a216a47576adc1f5
SamArgt <36817478+SamArgt@users.noreply.github.com>
2025-04-15 17:29:17 +0200

    Update __main__.py

--- src/rpy_motion_detector/__main__.py
+++ src/rpy_motion_detector/__main__.py
@@ -1,4 +1,25 @@
 if __name__ == "__main__":
+    import argparse
     from rpy_motion_detector.run import run
-
-    run()
+    parser = argparse.ArgumentParser(description="Motion Detector")
+    parser.add_argument(
+        "--config",
+        type=str,
+        help="Path to the configuration file",
+    )
+    parser.add_argument(
+        "--dry-run",
+        type=str,
+        help="Dry run mode, do not start the motion detector",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--log-output",
+        help=(
+            "Specify a file path for logging. If not provided, logs will be printed to stdout "
+            "unless configured otherwise."
+        ),
+        default=None,
+    )
+    args = parser.parse_args()
+    run(args.config, args.dry_run, args.log_output)
